6d5ce7470d72b77bbcfaf39c8b7a58ccf3243a45
IceTeaaaaa <tang.1251@osu.edu>
2019-07-08 12:03:30 +0800

    update

--- public/fetch.js
+++ public/fetch.js
@@ -26,6 +26,18 @@ async function refresh(){
     }
 }
 
+function load(name) {
+    let xhr = new XMLHttpRequest(),
+        okStatus = document.location.protocol === "file:" ? 0 : 200;
+    xhr.open('GET', name, false);
+    xhr.overrideMimeType("text/html;charset=utf-8");//默认为utf-8
+    xhr.send(null);
+    return xhr.status === okStatus ? xhr.responseText : null;
+}
+
+let text = load("weblist.txt");
+console.log(text);
+
 
 
 // async function onDelete(event) {
