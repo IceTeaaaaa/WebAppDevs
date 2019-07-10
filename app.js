4fb93c3427ca832bacde166d86eff6ba5f87c0a2
IceTeaaaaa <tang.1251@osu.edu>
2019-07-10 15:00:20 +0800

    update

---
+++ app.js
@@ -0,0 +1,18 @@
+var redis = require("redis"),
+    client = redis.createClient();
+
+// if you'd like to select database 3, instead of 0 (default), call
+// client.select(3, function() { /* ... */ });
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+client.SMEMBERS("updated_hrefs_https://www.wired.com/",redis.print);
+client.hkeys("hash key", function (err, replies) {
+    console.log(replies.length + " replies:");
+    replies.forEach(function (reply, i) {
+        console.log("    " + i + ": " + reply);
+    });
+    client.quit();
+});
