51439c721fd0bdfb70cea9e9f71e12bbddd61c1b
IceTeaaaaa <tang.1251@osu.edu>
2019-07-11 16:38:39 +0800

    update

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
 let redis = require("redis"),
     client = redis.createClient();
-let news_array = new Array();
-
+let updated_link_dic = new Array();
 
 client.on("error", function (err) {
     console.log("Error " + err);
 });
 
 client.multi().keys('*', function (err, replies) {
-        let updatedUrlArr = searchUpdated(replies,"updated_hrefs_");
-        console.log(updatedUrlArr);
-    }).exec(function (err, replies) {});
-
-
-client.SMEMBERS("updated_hrefs_https://www.wired.com/",function (err, reply) {
-    news_array = reply;
-    client.quit();
+    let updatedUrlArr = searchUpdated(replies,"updated_hrefs_https:");
+    for(let i = 0; i < updatedUrlArr.length; i++){
+        let link_title = new Array();
+        client.SMEMBERS(updatedUrlArr[i],function (err, reply) {
+            for(let j = 0; j < reply.length; j++){
+                client.GET('updated_hrefs_title_' + reply[j],function (err, title) {
+                    link_title[reply[j]] = title;
+                    client.quit();
+                })
+            }
+        });
+        updated_link_dic[updatedUrlArr[i]] = link_title;
+        console.log(updated_link_dic);
+    }
+    }).exec(function (err, replies) {
 });
+
+//
+// client.SMEMBERS("updated_hrefs_https://www.wired.com/",function (err, reply) {
+//     news_array = reply;
+//     console.log(reply);
+//     client.quit();
+// });
 
 function searchUpdated(arr,str){
     let newArr = [];
