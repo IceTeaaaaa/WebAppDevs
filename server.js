fd3f9793412b8ffc3e7961c2af9b9de6c0df576c
stevewang <596899832@qq.com>
2019-07-11 15:03:54 +0800

    update

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const exphbs  = require('express-handlebars');
-
+var path = require('path');
 const index = require('./routes/index.js');
 const views = require('./routes/views.js');
 
 const app = express();
-const hbs = exphbs.create({ defaultLayout: '../index' });
+const hbs = exphbs.create({
+  layoutsDir: path.join(__dirname, "views"),
+  defaultLayout: '',
+  extname: 'handlebars'
+});
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.set('view options', { layout: 'other' });
-
 
 // app.set('views', path.join(__dirname, 'views'));
 
@@ -45,64 +48,3 @@ async function startServer() {
   console.log('Listening on port 3000');
 }
 startServer();
-
-
-
-
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const MongoClient = require('mongodb').MongoClient;
-//
-// const app = express();
-// const jsonParser = bodyParser.json();
-//
-// app.use(express.static('public'));
-//
-// const DATABASE_NAME = 'eng-dict2';
-// const MONGO_URL = `mongodb://localhost:27017/${DATABASE_NAME}`;
-//
-// let db = null;
-// let collection = null;
-//
-// async function startServer() {
-//   // Set the db and collection variables before starting the server.
-//   db = await MongoClient.connect(MONGO_URL);
-//   collection = db.collection('words');
-//   // Now every route can safely use the db and collection objects.
-//   await app.listen(3000);
-//   console.log('Listening on port 3000');
-// }
-// startServer();
-//
-// ////////////////////////////////////////////////////////////////////////////////
-//
-// async function onLookupWord(req, res) {
-//   const routeParams = req.params;
-//   const word = routeParams.word;
-//
-//   const query = { word: word.toLowerCase() };
-//   const result = await collection.findOne(query);
-//
-//   const response = {
-//     word: word,
-//     definition: result ? result.definition : ''
-//   };
-//   res.json(response);
-// }
-// app.get('/lookup/:word', onLookupWord);
-//
-// async function onSetWord(req, res) {
-//   const routeParams = req.params;
-//   const word = routeParams.word.toLowerCase();
-//   const definition = req.body.definition;
-//
-//   const query = { word: word };
-//   const newEntry = { word: word, definition: definition };
-//   const params = { upsert: true };
-//   const response =
-//       await collection.update(query, newEntry, params);
-//
-//   res.json({ success: true });
-// }
-//
-// app.post('/set/:word', jsonParser, onSetWord);
