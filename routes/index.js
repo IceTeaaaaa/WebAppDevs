bcb077e8baf932e9e0e364c9fd8bc06f80c2514c
Pwc <p6cheng@ucsd.edu>
2019-07-16 11:46:49 +0800

    Revert "Merge branch 'master' of /Users/Pwc/Documents(local)/Github/WebAppDevs with conflicts."
    This reverts commit 1d43033bbaed55992409295d04b0ede6958c5d02.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,19 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
+var redis = require("redis"),
+    client = redis.createClient();
+
+let urls = "";
+let urls_array = new Array();
+
+let dic_url_suburl = new Array();
+let dic_suburl_title = new Array();
+let title_array = new Array();
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
 
 function find_title(results){
     let result_arr = new Array();
@@ -19,9 +32,6 @@ function find_title(results){
             // }
         }
     }
-
-    // if(`${results.url}`)
-
 }
 
 async function onViewIndex(req, res) {
@@ -35,29 +45,85 @@ async function onViewIndex(req, res) {
     // const u_arr = await req.collection.findOne(query);
     // console.log("11111111"+u_arr);
 
-    let a = await req.collection.findOne({type:'title'}).url_array;
-    await console.log("123" + a);
+    const a = await req.collection.find().toArray();
+    for(let b of a){
+        urls = `${b.url_array}`;
+    }
+    urls_array = urls.split(",");
+    console.log("urls array: " + urls_array);
+    let abc = "";
+    if(urls_array){
+        console.log("urls array length: " + urls_array.length);
+        for(let i = 0; i < urls_array.length; i++){
+            let sub_url_array = new Array();
+            client.SMEMBERS('updated_hrefs_' + urls_array[i],async function (err, reply) {
+               if(reply){
+                   console.log("reply: " + reply);
+                   console.log("reply number: " + reply.length);
 
+                   for(let j = 0; j < reply.length; j++){
+                       sub_url_array[j] = reply[j];
+                       await client.GET('updated_hrefs_title_' + reply[j], async function (err, title) {
 
-    var abc = ['1', '6'];
+                           if(title == null){
+                               console.log("title: title shi kong de" + title);
+                           }else{
+                               console.log("title: " + title);
+                               dic_suburl_title[sub_url_array[j]] = title;
+                           }
+
+                           // // req.collection.insert({"url": news});
+                           // // console.log(123);
+                           //  if(title == null){
+                           //  // db.webapp.findOne({"type":"title"}).url_array[0]
+                           //      req.collection.insert({"index": i, "url": urls[i], "sub_url": reply[j], "title": reply[j]});
+                           //
+                           //  }else{
+                           //      req.collection.insert({"index": i, "url": urls[i], "sub_url": reply[j], "title": title});
+                           //  }
+                            client.quit();
+                       })
+
+                   }
+                   dic_url_suburl[urls_array[i]] = sub_url_array;
+                   console.log("sub_url_array: " + sub_url_array);
+                   console.log("sub_url_array length: " + sub_url_array.length);
+               }
+            })
+        }
+        console.log("urls_array[0]: " + urls_array[0]);
+        console.log("url_array[urls_array[0]]: " + dic_url_suburl[urls_array[0]]);
+        console.log("urls_array[1]: " + urls_array[1]);
+        console.log("url_array[urls_array[1]]: " + dic_url_suburl[urls_array[1]]);
+        console.log("urls_array[2]: " + urls_array[2]);
+        console.log("url_array[urls_array[2]]: " + dic_url_suburl[urls_array[2]]);
+    }
+
     const placeholders = {
-        title_0: "123",
-        title_1: "fuck",
-        title: abc,
-        webpage: "http://www.baidu.com",
-        val: "aaaaa",
+        title_0: urls_array[0],
+        title: dic_url_suburl[urls_array[0]]
+        // title_0: urls_array[0],
+        // news_0_0: url_array[urls_array[0]][0],
+        // news_0_1: url_array[urls_array[0]][1]
     };
-
-    var source   = document.getElementById('text-template').innerHTML;
-    var template = Handlebars.compile(source);
-    var html    = template(data);
-
-    document.getElementById("text").innerHTML = html;
-
-
     res.render('index', placeholders);
 }
 router.get('/', onViewIndex);
+
+function searchUpdated(arr,str){
+    let newArr = [];
+    for(let element in arr){
+        if(countSubstr(arr[element],str) > 0){
+            newArr.push(arr[element]);
+        }
+    }
+    return newArr;
+}
+
+function countSubstr(str, substr) {
+    let reg = new RegExp(substr, "g");
+    return str.match(reg) ? str.match(reg).length : 0;
+}
 
 
 module.exports = router;
