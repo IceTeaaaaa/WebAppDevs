8279c02f857fa218e172ad69c6ca5b1492ef6f17
Pwc <p6cheng@ucsd.edu>
2019-07-16 18:03:17 +0800

    Dynamic web content
    - dynamically display webpages
    
    todo:
    - fix subtitles and urls

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const jsonParser = bodyParser.json();
 var redis = require("redis"),
     client = redis.createClient();
 
+var webpages = [];
+
 let urls = "";
 let urls_array = new Array();
 
@@ -13,7 +15,7 @@ let dic_suburl_title = new Array();
 let title_array = new Array();
 
 client.on("error", function (err) {
-    console.log("Error " + err);
+    // console.log("Error " + err);
 });
 
 function find_title(results){
@@ -50,26 +52,26 @@ async function onViewIndex(req, res) {
         urls = `${b.url_array}`;
     }
     urls_array = urls.split(",");
-    console.log("urls array: " + urls_array);
+    // console.log("urls array: " + urls_array);
     let abc = "";
     if(urls_array){
-        console.log("urls array length: " + urls_array.length);
+        // console.log("urls array length: " + urls_array.length);
         for(let i = 0; i < urls_array.length; i++){
             let sub_url_array = new Array();
             client.SMEMBERS('updated_hrefs_' + urls_array[i],async function (err, reply) {
                if(reply){
-                   console.log("reply: " + reply);
-                   console.log("reply number: " + reply.length);
+                   // console.log("reply: " + reply);
+                   // console.log("reply number: " + reply.length);
 
                    for(let j = 0; j < reply.length; j++){
                        sub_url_array[j] = reply[j];
                        await client.GET('updated_hrefs_title_' + reply[j], async function (err, title) {
 
                            if(title == null){
-                               console.log("title: title shi kong de" + title);
+                               // console.log("title: title shi kong de" + title);
                                dic_suburl_title[sub_url_array[j]] = sub_url_array[j];
                            }else{
-                               console.log("title: " + title);
+                               // console.log("title: " + title);
                                dic_suburl_title[sub_url_array[j]] = title;
                            }
 
@@ -87,38 +89,99 @@ async function onViewIndex(req, res) {
 
                    }
                    dic_url_suburl[urls_array[i]] = sub_url_array;
-                   console.log("sub_url_array: " + sub_url_array);
-                   console.log("sub_url_array length: " + sub_url_array.length);
+                   // console.log("sub_url_array: " + sub_url_array);
+                   // console.log("sub_url_array length: " + sub_url_array.length);
                }
             })
         }
-        console.log("urls_array[0]: " + urls_array[0]);
-        console.log("dic_url_suburl[urls_array[0]]: " + dic_url_suburl[urls_array[0]]);
-        console.log("urls_array[1]: " + urls_array[1]);
-        console.log("dic_url_suburl[urls_array[1]]: " + dic_url_suburl[urls_array[1]]);
-        console.log("urls_array[2]: " + urls_array[2]);
-        console.log("dic_url_suburl[urls_array[2]]: " + dic_url_suburl[urls_array[2]]);
+        // console.log("urls_array[0]: " + urls_array[0]);
+        // console.log("dic_url_suburl[urls_array[0]]: " + dic_url_suburl[urls_array[0]]);
+        // console.log("urls_array[1]: " + urls_array[1]);
+        // console.log("dic_url_suburl[urls_array[1]]: " + dic_url_suburl[urls_array[1]]);
+        // console.log("urls_array[2]: " + urls_array[2]);
+        // console.log("dic_url_suburl[urls_array[2]]: " + dic_url_suburl[urls_array[2]]);
+
+        for await(url of urls_array) {
+            console.log(url);
+            let mainSite = url;
+            let siteName = url;  // TODO: SPLIT STRING!
+            let subSites = [];
+            for(subsiteUrl in dic_url_suburl[url]) {
+                let one = {
+                    "title": dic_suburl_title[subsiteUrl],
+                    "url": subsiteUrl
+                };
+                subSites.push(one);
+            }
+
+            let entry = {
+                "mainSite": mainSite,
+                "siteName": siteName,
+                "subSites": subSites
+            };
+            await webpages.push(entry);
+        }
+
     }
 
+
+    // console.log("sfgsdfgsdgfsdgsdgdfsgfg");
+    // console.log(urls_array[0]);
+    // console.log('adsf');
     const placeholders = {
-        title_0: dic_url_suburl[urls_array[0]],
+        url_0: urls_array,
+        // title_0: dic_url_suburl[urls_array[0]],
         // url_1: urls_array[1],
-        title_1: dic_url_suburl[urls_array[1]],
-        // url_2: urls_array[2],
-        title_2: dic_url_suburl[urls_array[2]],
-        // url_3: urls_array[3],
-        title_3: dic_url_suburl[urls_array[3]],
-        // url_4: urls_array[4],
-        title_4: dic_url_suburl[urls_array[4]],
-        // url_5: urls_array[5],
-        title_5: dic_url_suburl[urls_array[5]],
-        numberOfCards: [0,1,2,3]
+        // title_1: dic_url_suburl[urls_array[1]],
+        // // url_2: urls_array[2],
+        // title_2: dic_url_suburl[urls_array[2]],
+        // // url_3: urls_array[3],
+        // title_3: dic_url_suburl[urls_array[3]],
+        // // url_4: urls_array[4],
+        // title_4: dic_url_suburl[urls_array[4]],
+        // // url_5: urls_array[5],
+        // title_5: dic_url_suburl[urls_array[5]],
+        numberOfCards: [0,1],
+
+        newTest: webpages,
+
+        test: [
+            {
+                mainSite: "https://www.bbc.com",
+                siteName: "bbc",
+                subSites: [
+                    {
+                        title: "Congresswomen hit back after Trump's tweets branded racist",
+                        url: "https://www.bbc.com/news/world-us-canada-48998696"
+                    },
+                    {
+                        title: "Times when Americans were told to 'go home' or 'go back'",
+                        url: "https://www.bbc.com/news/world-us-canada-48993211"
+                    },
+                ]
+            },
+            {
+                mainSite: "https://www.cnn.com",
+                siteName: "cnn",
+                subSites: [
+                    {
+                        title: "Congresswomen hit back after Trump's tweets branded racist",
+                        url: "https://www.cnn.com/2019/07/15/politics/gop-rebuke-trump-tweet/index.html"
+                    },
+                    {
+                        title: "Feds warn UFO enthusiasts against storming Area 51: The military 'stands ready'",
+                        url: "https://www.bbc.com/news/world-us-canada-48993211"
+                    },
+                ]
+            }
+        ],
 
         // title_0: urls_array[0],
         // news_0_0: url_array[urls_array[0]][0],
         // news_0_1: url_array[urls_array[0]][1]
     };
-    res.render('index', placeholders);
+    console.log(webpages[0]);
+    await res.render('index', placeholders);
 }
 router.get('/', onViewIndex);
 
