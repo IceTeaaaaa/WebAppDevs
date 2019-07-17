8d64579092b352e6ddd7a93389e1750a31c770ec
Pwc <p6cheng@ucsd.edu>
2019-07-17 11:38:26 +0800

    Revert "Revert "update""
    This reverts commit d1aab53d7a6109a9663f3997e336c8f61bd5442c.

--- server.js
+++ server.js
@@ -51,6 +51,10 @@ async function startServer() {
 
   collection = db.collection('webapp');
 
+  collection.remove({});
+  collection.insert({"type": "title","url_array": []});
+
+
   function setCollection(req, res, next) {
     req.collection = collection;
     next();
@@ -72,11 +76,16 @@ startServer();
 
 async function onApiUrl(req, res) {
 
-    collection.remove({});
-    const urls = req.body.url;
-    collection.insert({"type": "title","url_array": urls});
+  // collection.remove({});
+  const urls = req.body.url;
+  console.log("111123456765432123456urls: " + urls);
+  // collection.insert({"type": "title","url_array": urls});
+  const query = { type: "title" };
+  const newEntry = { type: "title", url_array: urls };
+  const params = { upsert: true };
+  const response =
+      await collection.update(query, newEntry, params);
+  res.json({ success: true });
 
 }
 app.post('/api', jsonParser, onApiUrl);
-
-
