9a442697d37778fa584db8c10d8b0579e635f9dc
IceTeaaaaa <tang.1251@osu.edu>
2019-07-19 11:42:20 +0800

    update hds

--- routes/index.js
+++ routes/index.js
@@ -121,22 +121,20 @@ router.get('/', onViewIndex);
 
 
 function webName(siteName){
-    console.log(siteName);
     let result = siteName.substring(0,siteName.length-5);
-    console.log(result);
+    //console.log(result);
     //http
     if(result.substring(0,8) === "https://"){
         result = siteName.substring(8, result.length);
     }else if(result.substring(0,7) === "http://"){
         result = siteName.substring(7, result.length);
     }
-    console.log(result);
     if(result.substring(0,3) === "www"){
         result = result.substring(4, result.length);
     }else if(result.substring(0,4) === "blog"){
         result = result.substring(5, result.length);
     }
-    console.log(result);
+    //console.log(result);
     return result;
 }
 
