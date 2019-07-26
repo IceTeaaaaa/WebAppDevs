dfc8fa16b71763f987537062c28bd0971327d599
IceTeaaaaa <tang.1251@osu.edu>
2019-07-26 11:19:32 +0800

    update hds

--- routes/index.js
+++ routes/index.js
@@ -12,18 +12,18 @@ const numOfRightUrls = 8;
 const numOfLeftUrls = 12;
 
 // Variables definitions
-let urls = "";
+//let urls = "";
 let urls_array = [];
 
-let righturls = "";
+//let righturls = "";
 let righturls_array = [];
 
 let dic_url_suburl = [];
 let dic_suburl_title = [];
 let dic_url_title = [];
 //let title_array = new Array();
-let usrname = "";
-let paswrd = "";
+let usrname = "default";
+let paswrd = "default";
 
 let default_url = [];
 let default_url_right = [];
@@ -77,34 +77,11 @@ router.post('/register/ID/', jsonParser, getID_Register);
 // Display the main page ('/')
 async function onViewIndex(req, res) {
     // in server, we call setCollection, which defines req.collection = collection (which is our mongodb database)
-    let a = null;
-    if(usrname === ""){
-        a = await req.collection.find().toArray();
-        for await(let b of a){
-            urls = `${b.url_array}`;
-            righturls =`${b.right_side_url}`
-        }
-    }else {
-        await req.collection.findOne({'username':usrname, 'password': paswrd}, async function(err, doc1){
-            if(!doc1){
-                await req.collection.findOne({'username':"default", 'password': "default"},  function (err, doc2) {
-                    if(doc2){
-                        a = doc2;
-                        urls = `${a.url_array}`;
-                        righturls =`${a.right_side_url}`;
-                    }
-                })
-            }else if(doc1){
-                a = doc1;
-                urls = `${a.url_array}`;
-                righturls =`${a.right_side_url}`;
-            }else if(err){
-                console.error("Username or password doesn't exist!");
-            }
-        });
-    }
-        // populate urls(cards view), and righturls(websites on the right to be added to the left)
-    // from mongodb, (specific for each user in the future).
+    //let a = null;
+    let a = await req.collection.findOne({'username':usrname, 'password': paswrd});
+    let urls = `${a.url_array}`;
+    let righturls =`${a.right_side_url}`;
+
     if(urls !== ""){
         urls_array = urls.split(",");
     }else{
@@ -219,7 +196,7 @@ async function onViewIndex(req, res) {
     // console.log(sideWebpages);
     const placeholders = {
         username: usrname === 'default' ? '' : usrname,
-        user_button: (usrname ==="" || usrname === "default") ? 'Log In' : 'Log Out',
+        user_button: (usrname === "default") ? 'Log In' : 'Log Out',
         cards: webpages,
         lists: sideWebpages,
     };
